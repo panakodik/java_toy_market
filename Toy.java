65b0a9a74395b078b87bfd656760a094fe545f8a
Pavlinov Aleksandr <pandom@yandex.ru>
2023-09-21 23:49:49 +0300

    2

--- Toy.java
+++ Toy.java
@@ -32,4 +32,22 @@ class Toy {
     private int id;
     private String name;
     private int frequency;
+
+    public Toy(int id, String name, int frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
 }
